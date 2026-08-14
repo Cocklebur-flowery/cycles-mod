577706bd8b80d5da983b4dc411d7f940f91538b3
Cocklebur-flowery <192856940+Cocklebur-flowery@users.noreply.github.com>
2026-08-14 08:26:38 +0800

    fix(vulkan): target final framebuffer render method

--- src/main/java/dev/cyclesrenderer/mixin/MinecraftMixin.java
+++ src/main/java/dev/cyclesrenderer/mixin/MinecraftMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
     @ModifyArg(
-            method = "runTick",
+            method = "renderFrame",
             at = @At(
                     value = "INVOKE",
                     target = "Lcom/mojang/blaze3d/systems/GpuSurface;blitFromTexture(Lcom/mojang/blaze3d/systems/CommandEncoder;Lcom/mojang/blaze3d/textures/GpuTextureView;)V"),
